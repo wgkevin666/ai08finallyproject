a6402a7c4aef3480f7fddd497574f56dbf13f0d2
kevin wnag <you@example.com>
2020-09-25 21:27:15 +0800

    a little bit Food_Recognition to json

--- src/index.js
+++ src/index.js
@@ -47,21 +47,6 @@ app.get("/openpose", function (request, response) {
     response.render("openpose");
 })
 
-app.get("/childpro", function (request, response) {
-    //const ls = spawn('ls', ['-lh', '/usr'])
-    const ls = spawn('python3', ['/opt/ai08finallyproject/function/Food_Recognition/predict.py', '-c', '/opt/ai08finallyproject/function/Food_Recognition/config.json', '-i', '/opt/ai08finallyproject/function/Food_Recognition/data/test/imgs/img_204.jpg', '-o', '/opt/ai08finallyproject/function/Food_Recognition/data/output/']);
-    ls.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
-
-    ls.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
-
-    ls.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-    });
-});
 // app.get('/childp', (req, res)=>{
 //     const ls = spawn('ls', ['-lh', '/usr']);
 
@@ -85,11 +70,22 @@ app.post('/try-upload', upload.single('food'), (req, res) => {
 
     //res.json(req.file);
 
-    const ls = spawn('python3', ['/opt/ai08finallyproject/function/Food_Recognition/predict.py', '-c', '/opt/ai08finallyproject/function/Food_Recognition/config.json', 
+    const ls = spawn('python3', ['/opt/ai08finallyproject/function/Food_Recognition/predict_show_fat.py', '-c', '/opt/ai08finallyproject/function/Food_Recognition/config.json', 
     '-i', req.file.path, 
     '-o', '/opt/ai08finallyproject/public/img-downloads/']);
+    
+    result = ''
+
     ls.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
+//	result += data
+        console.log(`[Nodejs] stdout: ${data}`);
+	d = data.toString().split('\n')
+	console.log(`[LIST] ${d}`)
+	d = d.slice(-2)
+	console.log(`[SLICE] ${d}`)
+	d = JSON.stringify(d)
+	console.log(`[JSON Object] ${d}`)
+//	res.json(`[LIST] ${d}`);
     });
 
     ls.stderr.on('data', (data) => {
@@ -97,6 +93,8 @@ app.post('/try-upload', upload.single('food'), (req, res) => {
     });
 
     ls.on('close', (code) => {
+//	result = result.split('\n')
+	//console.log('[RESULT] ' + result)
 //	   res.json('/opt/ai08finallyproject/public/img-downloads/' + req.file.filename);
 //        console.log(`child process exited with code ${code}`);
 //        res_img = Object.assign({}, req.file)
